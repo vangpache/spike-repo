7392cec2882eed35a7d19b09fec0522fe99878dd
pachevang <vangpache@gmail.com>
2019-10-05 11:51:10 -0500

    adds material-ui and grids to rearrange display of spikes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Moment from 'react-moment';
 // import SweetAlert from 'sweetalert2-react';
 // import { withSwalInstance } from 'sweetalert2-react';
 import swal from '@sweetalert/with-react';
+import { withStyles } from '@material-ui/styles';
+import { Card, CardContent, Grid, Paper } from '@material-ui/core';
 
 
 // const SweetAlert = withSwalInstance(swal);
@@ -45,21 +47,21 @@ class App extends Component {
       icon: "warning",
       buttons: true,
       dangerMode: true,
-        })
-        .then((willDelete) => {
-          if (willDelete) {
-            swal("poof! Your search commences!", {
-              icon: "success",
-            })
-            this.props.dispatch({
-              type: 'GET_BOOKS_SEARCHED',
-              payload: this.state.search
-            })
-          } else {
-            swal("your imaginary file is safe!");
-          }
-        })
-    
+    })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("poof! Your search commences!", {
+            icon: "success",
+          })
+          this.props.dispatch({
+            type: 'GET_BOOKS_SEARCHED',
+            payload: this.state.search
+          })
+        } else {
+          swal("your imaginary file is safe!");
+        }
+      })
+
     console.log('handleClick payload:', this.state.search);
 
   }
@@ -69,7 +71,7 @@ class App extends Component {
   //     pictures: this.state.pictures.concat(picture)
   //   })
   //   console.log('in onDrop:', this.state.pictures);
-    
+
   // }
 
   handleChange = (event) => {
@@ -89,60 +91,69 @@ class App extends Component {
     })
   }
 
-  
+
 
 
   render() {
 
-
-
-    
-
-    
-
     return (
-      <div className="App">
-        <h1>SPIKE APP</h1>
-        <br /><br /><br />
-        <p><b>This uploader doesn't work, it' just looks pretty(react-images-upload)</b></p>
-        <ImageUpload />
-        <br /><br /><br />
-        <div>
-          <p><b>upload an image to cloudinary</b></p>
-          <input type="file" name="file" onChange={this.handleChange} /><br/>
-          <button onClick={this.handleFileUpload} >upload</button>
-        </div>
-        <br/><br/><br/>
-        {/* {JSON.stringify(this.state)} */}
-        <h2>book getter test</h2>
-        <input type="text" placeholder="search book title or author" onChange={this.handleSearch} />
-        <button onClick={this.handleClick}>search</button>
-        <p><Moment format="LLLL">
-          1976-04-19T12:59-0500
-          </Moment></p>
+      <>
+        <div className="App">
+          <h1>SPIKE APP</h1>
+          <br /><br /><br />
+          {/* <p><b>This uploader doesn't work, it' just looks pretty(react-images-upload)</b></p>
+          <ImageUpload /> */}
 
-        {this.props.test.map(each => {
-          return (
-            <>
-          <p>{each.best_book.title._text}</p>
-          <p>{each.best_book.author.name._text}</p>
-          <img src={each.best_book.image_url._text} alt="insert description of book here" />
-              <p>{each.original_publication_year._text}</p>
-            </>
-          )
-        })}
-        <br/>
-        <br />
-        <br />
-        <div >
-          <p><b>Displaying the url from cloudinary upload</b></p>
-          <img className="image" src="http://res.cloudinary.com/dquorievt/image/upload/v1570288050/etakxagp8x8sbzqbmsy7.png" />
+          {/* {JSON.stringify(this.state)} */}
+
+          {/* {JSON.stringify(this.props.test)} */}
+
+        
+
+        <div>
+          <Grid container spacing={3}>
+            {/* <Grid item xs={12}> */}
+            <Grid item xs={4}>
+              <Grid item xs={12}>
+                <div>
+                  <p><b>upload an image to cloudinary</b></p>
+                  <input type="file" name="file" onChange={this.handleChange} /><br />
+                  <button onClick={this.handleFileUpload} >upload</button>
+                </div>
+              </Grid>
+              <Grid item xs={12}>
+                <div>
+                  <p><b>Displaying the url from cloudinary upload</b></p>
+                  <img className="image" src="http://res.cloudinary.com/dquorievt/image/upload/v1570288050/etakxagp8x8sbzqbmsy7.png" />
+                </div>
+              </Grid>
+            </Grid>
+            <Grid item xs={8}>
+              <div>
+                <h2>book getter test</h2>
+                <input type="text" placeholder="search book title or author" onChange={this.handleSearch} />
+                <button onClick={this.handleClick}>search</button>
+                <p><Moment format="LLLL">
+                  1976-04-19T12:59-0500
+                      </Moment></p>
+
+                {this.props.test.map(each => {
+                  return (
+                    <>
+                      <p>{each.best_book.title._text}</p>
+                      <p>{each.best_book.author.name._text}</p>
+                      <img src={each.best_book.image_url._text} alt="insert description of book here" />
+                      <p>{each.original_publication_year._text}</p>
+                    </>
+                  )
+                })}
+              </div>
+            </Grid>
+            {/* </Grid> */}
+          </Grid>
         </div>
-        
-          
-        {/* {JSON.stringify(this.props.test)} */}
-        
-      </div>
+        </div>
+      </>
     );
   }
 }
@@ -151,10 +162,10 @@ class App extends Component {
 
 
 const mapStateToProps = reduxStore => {
-  return{
+  return {
     test: reduxStore.books
   }
 }
 
 
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps)(App);
